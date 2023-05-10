e0e1eaf8885f3cc43d953cf1b5ace0f3a4dcb51e
Vinayak <doijadvinayak668@gmail.com>
2023-05-10 17:25:33 +0530

    insurance

--- src/main/java/com/insurance/model/Policy.java
+++ src/main/java/com/insurance/model/Policy.java
@@ -2,4 +2,34 @@ package com.insurance.model;
 
 public class Policy {
 
+	// id, name, city,
+
+	private int id;
+	private String name;
+	private String city;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
 }
