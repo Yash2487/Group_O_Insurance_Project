47b29b57e6d4baf2a1e5f579b90cd49eebdd44ba
Yash <yash.mate4494@gmail.com>
2023-05-11 09:44:43 +0530

    one method deleted

--- src/main/java/com/insurance/service/PasswordService.java
+++ src/main/java/com/insurance/service/PasswordService.java
@@ -3,8 +3,6 @@ package com.insurance.service;
 import com.insurance.model.Password;
 
 public interface PasswordService {
-	
-	public Password getPassword(int id);
 
 	public Password changePassword(int id);
 }
