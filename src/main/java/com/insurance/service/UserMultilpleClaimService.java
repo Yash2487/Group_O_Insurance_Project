1f3ba090779d81b86b98cb94815f5e58109c9ff2
Vinayak <doijadvinayak668@gmail.com>
2023-05-13 12:19:36 +0530

    userMultipleClaimService

---
+++ src/main/java/com/insurance/service/UserMultilpleClaimService.java
@@ -0,0 +1,9 @@
+package com.insurance.service;
+
+import com.insurance.model.UserMultipleClaim;
+
+public interface UserMultilpleClaimService {
+
+	public UserMultipleClaim saveUserMultipleClaim(UserMultipleClaim userMultipleClaim);
+
+}
