a9638fae9092a0409ba94538e572b1aab73d60d0
Yash <yash.mate4494@gmail.com>
2023-05-19 14:32:46 +0530

    Task 163 by Yash Mate

---
+++ src/main/java/com/insurance/service/BranchDetailsFetchService.java
@@ -0,0 +1,13 @@
+package com.insurance.service;
+
+/*
+ * Task 163: Design API to fetch branch details from system
+ * @author Yash Mate
+ */
+import com.insurance.model.BranchDetailsFetch;
+
+public interface BranchDetailsFetchService {
+
+	public BranchDetailsFetch getBranchDetailsFetch(int id);
+
+}
