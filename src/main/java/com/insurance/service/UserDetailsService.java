b7e1607eba6b4e90922f74dd84e3b0d5ec1438e5
abhijit <abhijitdaware77@gmail.com>
2023-05-19 03:00:47 -0400

    Task Completed By Abhijit Daware

---
+++ src/main/java/com/insurance/service/UserDetailsService.java
@@ -0,0 +1,8 @@
+package com.insurance.service;
+
+public interface UserDetailsService {
+	/*Task 170 Design Service to delete user with multiple nominee details into system ---
+	Task Completed By Abhijit Daware*/
+
+	public void deleteById(Integer id);
+}
