a784112199907c59fc4f204cb516254d8234affb
Yash <yash.mate4494@gmail.com>
2023-05-23 21:07:45 +0530

    Task 134-135 by Yash, Task 136 by Vinayak, Task 137 by Rohini, Task 138 by Abhijit

--- src/main/java/com/insurance/repository/UserInformationRepository.java
+++ src/main/java/com/insurance/repository/UserInformationRepository.java
@@ -6,10 +6,18 @@ import org.springframework.stereotype.Repository;
 import com.insurance.model.UserInformation;
 
 /*
- * Task- 138 Build the Restful API to delete the user details from database----------> Completed by Abhijeet
+ * Task 134- Build the Restful API to add user details into database ---------> Yash
+ * Task 135- Build the Restful API to update user details into database-------- > Yash
+ * Task 136- Build the Restful API to get user details from database --------> Vinayak
+ * Task 137- Build the Restful API to fetch all user details from database --------> Rohini
+ * Task 138- Build the Restful API to delete the user details from database----------> Abhijeet
  */
-
 @Repository
 public interface UserInformationRepository extends JpaRepository<UserInformation, Integer> {
+	// this will used to get user by id
+	public UserInformation getUserById(int id);
+
+	//
+	public UserInformation findById(int id);
 
 }
